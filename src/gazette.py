163e0c041e92fd57e593b84e92861437beb29771
GabrielTrettel <gabrielmtrettel@gmail.com>
2020-09-18 20:45:01 -0300

    Fixing a bug related with where the linear text was being incremented

--- src/gazette.py
+++ src/gazette.py
@@ -17,38 +17,39 @@ class Gazette:
         self.__split_cols()
 
     def each_page(self):
-       pages = []
-       page_buff = [] 
+        pages = []
+        page_buff = [] 
 
-       for line in self.file:
+        for line in self.file:
             page_buff.append(line)
             if '\014' in line:
-               pages.append(page_buff)
-               page_buff = []
+                pages.append(page_buff)
+                page_buff = []
+        
+        if len(page_buff) > 0:
+            pages.append(page_buff)
 
-       return pages
+        return pages
 
 
     def __split_cols(self):
         """ __split_cols
         Splits doc cols into a linear layout
         """
-        linear_text = ""
-        
         for i,page in enumerate(self.pages):
-            if self.pages_avg_col[i] >= 1.2 * self.total_avg_col or self.pages_avg_col[i] < 2:                      
-                
-                linear_text += "\n".join(page)
+            if self.pages_avg_col[i] >= 1.2 * self.total_avg_col or self.pages_avg_col[i] < 2:
+                #  print("\n".join(page))
+                #  linear_text += "\n".join(line for line in page) + "\n"
+                self.linear_text += str("".join(page))
                 continue
             
 
             page_in_cols = []
             footer = []
-            for j,line in enumerate(page[0]):
+            for j,line in enumerate(page):
                 line = line.strip()
                 #  print(line)
-                cols = list(map(lambda x: x.strip(), re.split(self.split_re, line)))
-
+                cols = self.split_line_naive_strategy(line)
                 if len(cols) == 1 and self.pages_avg_col[i] >= 2 and j > 0.9*len(page):
                     footer.append(" ".join(cols))
                 else:
@@ -68,6 +69,11 @@ class Gazette:
             self.linear_text += "\n".join(footer) + "\n\014\n"
             
 
+    def split_line_naive_strategy(self, line):
+        return list(map(lambda x: x.strip(), re.split(self.split_re, line)))
+
+    def split_line_local_strategy(self, lines):
+        pass
 
     def calc_avg_cols(self, page):
         cols_sum = 0
@@ -98,6 +104,7 @@ if __name__ == "__main__":
     with open(file, 'r') as f:
         g = Gazette(file, "sa", "10/0/0")
         print(g.pages_avg_col)
+        print(len(g.pages))
         print(g.linear_text)
 
 
