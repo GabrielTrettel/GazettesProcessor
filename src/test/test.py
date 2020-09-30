b08e4df0c854f2338de934f6e5192e6e4e8f6785
lubianat <tiago.lubiana.alves@usp.br>
2020-09-30 09:58:32 -0300

    test: Add basic test for consistency

--- src/test/test.py
+++ src/test/test.py
@@ -5,13 +5,22 @@ import os
 
 class TestStringMethods(unittest.TestCase):
     def setUp(self):
-        self.gp = Gazette('test/test_file.txt', '', '')
+        self.gp = Gazette('test/test_file.txt' , city="?", date="?")
 
 
     def test_get_list_of_pages(self):
         result = self.gp.get_list_of_pages()
         len_result = len(result)
         self.assertEqual(3, len_result, "A quantidade de paginas não retornou o valor correto")
+
+    def test_if_it_is_consistently_failing(self):
+        result = self.gp.linear_text
+        output_file = 'test/test_output.txt'
+        with open(output_file, 'r') as f:
+                expected = f.read()
+
+        self.assertEqual(result, expected, "Something changed")
+
 
 
 
